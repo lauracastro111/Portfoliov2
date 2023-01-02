e7daee14ce841965586705188ae024febcca4d2b
Laura Castro <laura.castro@hotmail.es>
2023-01-02 18:43:05 -0500

    skill page and work page

--- js/script.js
+++ js/script.js
@@ -44,3 +44,21 @@ laura.addEventListener("mouseleave", () => {
     document.querySelector(".right-side .text-laura a").innerText = "Laura"
 
 })
+
+
+// script for cricle
+const circles = document.querySelectorAll('.element')
+
+const n = 6;  // numero de circulos
+const r = 120 // radio
+
+let angulo = 0;
+let originX = circles[0].offsetLeft
+let originY = circles[0].offsetTop
+
+setInterval(() => {
+ angulo += 0.01
+ circles.forEach((element,i) =>{
+ element.style.left = `${originX + r*Math.cos(angulo + 2*Math.PI/n*i)}px`
+ element.style.top = `${originY + r*Math.sin(angulo + 2*Math.PI/n*i)}px`})
+},20)
