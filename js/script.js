5a4304017352776b8db0e070091b4fbe1e703bef
Andres Correa <andrescorrea.m@hotmail.com>
2023-01-25 10:45:40 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,20 +14,12 @@ function mouseEnterOrLeave(element, text, enter = false) {
   element.firstElementChild.innerText = text;
 }
 
-hiThere.addEventListener("mouseenter", () =>
-  mouseEnterOrLeave(hiThere, "About", true)
-);
-hiThere.addEventListener("mouseleave", () =>
-  mouseEnterOrLeave(hiThere, "Hi There!")
-);
+hiThere.addEventListener("mouseenter", () =>  mouseEnterOrLeave(hiThere, "About", true));
+hiThere.addEventListener("mouseleave", () =>  mouseEnterOrLeave(hiThere, "Hi There!"));
 im.addEventListener("mouseenter", () => mouseEnterOrLeave(im, "Work", true));
 im.addEventListener("mouseleave", () => mouseEnterOrLeave(im, "I'm"));
-laura.addEventListener("mouseenter", () =>
-  mouseEnterOrLeave(laura, "Skills", true)
-);
+laura.addEventListener("mouseenter", () =>  mouseEnterOrLeave(laura, "Skills", true));
 laura.addEventListener("mouseleave", () => mouseEnterOrLeave(laura, "Laura"));
-
-
 
 //----- Function to manage the lenguage ----
 const texts = [
@@ -35,19 +27,52 @@ const texts = [
     id: "it7",
     eng: `I am a social media & web designer, passionate about what I do. \n I believe that even the impossible is possible. \n You put the limits!`,
     fra: `Je suis une marketing des médias sociaux et une conceptrice de sites Web, passionné par ce que je fais.\n Je crois que même l'impossible est possible.\n C'est vous qui fixez les limites !`
+  },
+  {
+    id: "it8",
+    eng: `Resume`,
+    fra: `Curriculum Vitae`
   }
 ];
 const checkBox = document.querySelector(".form-check input");
 const alltexts = document.querySelectorAll(".translate");
 
 const switchLenguage = () => {
+  
+   if (checkBox.checked) {
+     
+      mouseEnterOrLeave(hiThere, "À propos de moi", true);
+      mouseEnterOrLeave(hiThere, "Bonjour");
+      mouseEnterOrLeave(im, "Mon Travail", true);
+      mouseEnterOrLeave(im, "Je suis");
+      mouseEnterOrLeave(laura, "Compétences", true);
+     
+      hiThere.addEventListener("mouseenter", () =>  mouseEnterOrLeave(hiThere, "À propos de moi", true));
+      hiThere.addEventListener("mouseleave", () =>  mouseEnterOrLeave(hiThere, "Bonjour"));
+      im.addEventListener("mouseenter", () => mouseEnterOrLeave(im, "Mon Travail", true));
+      im.addEventListener("mouseleave", () => mouseEnterOrLeave(im, "Je suis"));
+      laura.addEventListener("mouseenter", () =>  mouseEnterOrLeave(laura, "Compétences", true));
+          
+   }else{
+      mouseEnterOrLeave(hiThere, "About", true);
+      mouseEnterOrLeave(hiThere, "Hi There!");
+      mouseEnterOrLeave(im, "Work", true);
+      mouseEnterOrLeave(im, "I'm");
+      mouseEnterOrLeave(laura, "Skills", true);
+         
+     
+      hiThere.addEventListener("mouseenter", () =>  mouseEnterOrLeave(hiThere, "About", true));
+      hiThere.addEventListener("mouseleave", () =>  mouseEnterOrLeave(hiThere, "Hi There!"));
+      im.addEventListener("mouseenter", () => mouseEnterOrLeave(im, "Work", true));
+      im.addEventListener("mouseleave", () => mouseEnterOrLeave(im, "I'm"));
+      laura.addEventListener("mouseenter", () =>  mouseEnterOrLeave(laura, "Skills", true));
+   }
+    
   [...alltexts].forEach((ele) => {
     let text = texts.find((txt) => ele.id === txt.id);
     if (checkBox.checked) {
-      console.log("French");
       ele.innerText = text.fra;
     } else {
-      console.log("English");
       ele.textContent = text.eng;
     }
   });
