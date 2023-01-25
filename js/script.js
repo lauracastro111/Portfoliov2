01b6019499cb385b67d67f79b169e96e52aee5c7
Andres Correa <andrescorrea.m@hotmail.com>
2023-01-25 09:52:59 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,57 @@
+// ----Logic for Index.html page to manage the dynamic name-----
 const hiThere = document.querySelector(".right-side .text-hi");
 const im = document.querySelector(".right-side .text-im");
 const laura = document.querySelector(".right-side .text-laura");
 
-
-function mouseEnterOrLeave(element, text, enter=false) {
-	if(enter){
-		element.classList.remove("dynamicTitleOff");
-		element.classList.add("dynamicTitleOn");
-	}
-	else{
-		element.classList.remove("dynamicTitleOn");
-		element.classList.add("dynamicTitleOff");
-	}
-	element.innerText.firstElementChild = text;
+function mouseEnterOrLeave(element, text, enter = false) {
+  if (enter) {
+    element.classList.remove("dynamicTitleOff");
+    element.classList.add("dynamicTitleOn");
+  } else {
+    element.classList.remove("dynamicTitleOn");
+    element.classList.add("dynamicTitleOff");
+  }
+  element.innerText.firstElementChild = text;
 }
 
-hiThere.addEventListener("mouseenter", () => mouseEnterOrLeave(hiThere,"About", true))
-im.addEventListener("mouseenter", () => mouseEnterOrLeave(im,"Work", true))
-laura.addEventListener("mouseenter", () => mouseEnterOrLeave(laura,"Skills", true))
+hiThere.addEventListener("mouseenter", () =>
+  mouseEnterOrLeave(hiThere, "About", true)
+);
+hiThere.addEventListener("mouseleave", () =>
+  mouseEnterOrLeave(hiThere, "Hi There!")
+);
+im.addEventListener("mouseenter", () => mouseEnterOrLeave(im, "Work", true));
+im.addEventListener("mouseleave", () => mouseEnterOrLeave(im, "I'm"));
+laura.addEventListener("mouseenter", () =>
+  mouseEnterOrLeave(laura, "Skills", true)
+);
+laura.addEventListener("mouseleave", () => mouseEnterOrLeave(laura, "Laura"));
 
-hiThere.addEventListener("mouseleave", () => mouseEnterOrLeave(hiThere,"Hi There!"))
-im.addEventListener("mouseleave", () => mouseEnterOrLeave(im,"I'm"))
-laura.addEventListener("mouseleave", () => mouseEnterOrLeave(laura,"Laura"))
+
+
+//----- Function to manage the lenguage ----
+const texts = [
+  {
+    id: "it7",
+    eng: `I am a social media & web designer, passionate about what I do. <br /> I believe that even the impossible is possible. <br /> You put the limits!`,
+    fra: `Je suis une marketing des médias sociaux et une conceptrice de sites Web, passionné par ce que je fais. <br /> Je crois que même l'impossible est possible. <br /> C'est vous qui fixez les limites !`
+  }
+];
+const checkBox = document.querySelector(".form-check input");
+const alltexts = document.querySelectorAll(".translate");
+
+const switchLenguage = () => {
+  [...alltexts].forEach((ele) => {
+    let text = texts.find((txt) => ele.id === txt.id);
+    if (checkBox.checked) {
+      console.log("French");
+      // ele.innerHtml = text.fra;
+      ele.innerText = text.fra;
+    } else {
+      console.log("English");
+      ele.innerText = text.eng;
+    }
+  });
+};
+
+checkBox.addEventListener("change", switchLenguage);
